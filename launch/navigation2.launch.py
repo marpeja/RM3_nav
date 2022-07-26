dbe90630e80dbee133611af0394c6dda6477f25e
marpeja <jaime.martinez.perez@alumnos.upm.es>
2022-07-26 12:08:07 +0200

    First commit Message

---
+++ launch/navigation2.launch.py
@@ -0,0 +1,57 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+	pkg_rm3_nav = get_package_share_directory('nav_rm3')
+	pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
+	namespace = LaunchConfiguration('namespace', default='')
+	use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+	with_rviz = LaunchConfiguration('with_rviz', default='true')
+	nav2_config_file = LaunchConfiguration('params', 
+						default=os.path.join(pkg_rm3_nav, 'config', 'nav2.yaml'))
+	rviz_config_file = LaunchConfiguration('rviz_config',
+						default=os.path.join(pkg_nav2_bringup, 'rviz', 'nav2_default_view.rviz'))
+
+	# nav2 bringup launch file
+	nav2_bringup_launch_file = IncludeLaunchDescription(
+					PythonLaunchDescriptionSource(os.path.join(pkg_nav2_bringup, 'launch', 'navigation_launch.py')),
+					launch_arguments={
+						'use_sim_time': use_sim_time,
+						'params_file': nav2_config_file,
+						'namespace': namespace,
+                              		'use_lifecycle_mgr': 'false',
+                              		'map_subscribe_transient_local': 'true'}.items(),
+					)
+
+	# rviz2
+	rviz2 = Node(package='rviz2', executable='rviz2',
+					name='rviz2',
+					arguments=['-d', rviz_config_file],
+					parameters=[{'use_sim_time': use_sim_time}],
+					output='screen',
+					condition=IfCondition(with_rviz))	
+	
+	return LaunchDescription([
+		DeclareLaunchArgument('namespace',
+       		 	default_value='',
+        			description='Top-level namespace'),
+		DeclareLaunchArgument('params_file',
+		     		default_value=nav2_config_file,
+		     		description="Path to nav2 config file"),
+		DeclareLaunchArgument('use_sim_time',
+                                default_value=use_sim_time,
+                                description="Use sim clock or not"),
+		DeclareLaunchArgument('with_rviz',
+								default_value=with_rviz,
+								description='Launch rviz2'),
+		nav2_bringup_launch_file,
+		rviz2
+		])
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'nav_rm3'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jaime',
+    maintainer_email='jaime@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
